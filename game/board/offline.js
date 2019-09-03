7a432dd6e6890e2ad06aa14b4542a21874fc01b5
fabiano tederiche <fabiano_tederiche1@hotmail.com>
2019-09-02 22:14:17 -0300

    version 0.0.1

---
+++ game/board/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1566327598,
+	"fileList": [
+		"data.js",
+		"game.html",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/dado-sheet0.png",
+		"images/sprite10-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite11-sheet0.png",
+		"images/amarelohorizont-sheet0.png",
+		"images/azulhorizont-sheet0.png",
+		"images/verdehorizonte-sheet0.png",
+		"images/pino-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
